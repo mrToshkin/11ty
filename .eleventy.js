4f8d8183bf30301f2221af510ec7cc625fa4437d
mrToshkin <toshkin.na@gmail.com>
2021-07-04 21:57:22 +0400

    init

---
+++ .eleventy.js
@@ -0,0 +1,42 @@
+const htmlmin = require('html-minifier');
+const markdown = require('markdown-it')({ html: true });
+
+const htmlminConfig = {
+  removeComments: true,
+  collapseWhitespace: true
+}
+
+module.exports = (config) => {
+  config.addPairedShortcode('markdown', (content) => {
+    return markdown.render(content);
+  });
+
+  config.addFilter('htmlmin', (value) => (
+    htmlmin.minify(value, htmlminConfig)
+  ));
+
+  config.addTransform('htmlmin', (content, outputPath) => {
+    if(outputPath && outputPath.endsWith('.html')) {
+      return htmlmin.minify(content, htmlminConfig);
+    }
+
+    return content;
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'build',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'releases'
+    },
+    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    templateFormats: [
+      'md', 'njk'
+    ],
+  };
+};
---
+++ src/releases/1/index.md
@@ -0,0 +1,6 @@
+---
+title: 'title from md'
+layout: index.njk
+---
+
+# markdown
