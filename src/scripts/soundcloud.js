9fcfe68f076f0d8c776c1c3a4e1c52cd9d6e205a
mrToshkin <toshkin.na@gmail.com>
2021-07-18 16:16:20 +0400

    Update 2021-07-18T12:16:02.026Z

--- src/scripts/soundcloud.js
+++
@@ -1,7 +0,0 @@
-const $soundcloud = document.querySelector('.soundcloud__player');
-const $button = document.querySelector('.soundcloud__button');
-
-$button.addEventListener('click', () => {
-  $soundcloud.classList.toggle('soundcloud__player--opened')
-  $button.classList.toggle('soundcloud__button--opened')
-})
